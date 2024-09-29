f3b6548f961922efc9107a65c9da846b2711bbe3
Prajwal Kumar <prasunjaysjc@gmail.com>
2024-09-29 09:00:20 +0530

    finalc

--- backend/modelfiles/model.py
+++
@@ -1,7 +0,0 @@
-# Available backend options are: "jax", "tensorflow", "torch".
-import os
-os.environ["KERAS_BACKEND"] = "tensorflow"
-	
-import keras
-
-model = keras.saving.load_model("hf://inzamamalii/image_forgery_detection")
